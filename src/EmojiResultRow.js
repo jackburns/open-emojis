898268c05922f9db16aaf78b5471808b372914de
Jack Burns <jack@raptiq.com>
2018-02-26 14:41:46 -0800

    remove emoji fetching

--- src/EmojiResultRow.js
+++ src/EmojiResultRow.js
@@ -1,25 +1,19 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import './EmojiResultRow.css';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import "./EmojiResultRow.css";
 
 class EmojiResultsRow extends PureComponent {
   render() {
-    const codePointHex = this.props.symbol.codePointAt(0).toString(16);
-    const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
+    const src = this.props.symbol;
     return (
-      <div 
+      <div
         className="component-emoji-result-row copy-to-clipboard"
         data-clipboard-text={this.props.symbol}
       >
-        <img
-          alt={this.props.title}
-          src={src}
-        />
-        <span
-          className="title"
-        >
-          {this.props.title}
+        <span alt={this.props.title} className="emoji">
+          {src}
         </span>
+        <span className="title">{this.props.title}</span>
       </div>
     );
   }
